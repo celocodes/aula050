6f55e3cdfbbdc3cf3c8fde365b5a3f7795490db9
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-03-07 20:46:06 -0300

    teste4

--- hello.py
+++ hello.py
@@ -18,7 +18,7 @@ class NameForm(FlaskForm):
     name = StringField('Informe o seu nome:', validators=[DataRequired()])
     sobrenome = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
     inst = StringField('Informe a sua instituição de ensino:', validators=[DataRequired()])
-    disciplina = SelectField('Informe a sua disciplina:', choices=[('dsw', 'DSWA5'), ('dwb', 'DWBA4'), ('gest', 'Gestão de projetos')], validators=[DataRequired())
+    #disciplina = SelectField('Informe a sua disciplina:', choices=[('dsw', 'DSWA5'), ('dwb', 'DWBA4'), ('gest', 'Gestão de projetos')], validators=[DataRequired())
     submit = SubmitField('Submit')
 
 
