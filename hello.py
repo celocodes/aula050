63ef0e7e6392b648f1fa2626a37cee59fea264e1
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-03-07 20:51:51 -0300

    teste7

--- hello.py
+++ hello.py
@@ -15,10 +15,10 @@ url = request.host_url
 ip = request.remote_addr
 
 class NameForm(FlaskForm):
-    name = StringField('Informe o seu nome:', validators=[DataRequired()])
+    nome = StringField('Informe o seu nome:', validators=[DataRequired()])
     sobrenome = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
     inst = StringField('Informe a sua instituição de ensino:', validators=[DataRequired()])
-    #disciplina = SelectField('Informe a sua disciplina:', choices=[('dsw', 'DSWA5'), ('dwb', 'DWBA4'), ('gest', 'Gestão de projetos')], validators=[DataRequired())
+    disciplina = SelectField('Informe a sua disciplina:', choices=[('dsw', 'DSWA5'), ('dwb', 'DWBA4'), ('gest', 'Gestão de projetos')], validators=[DataRequired())
     submit = SubmitField('Submit')
 
 
