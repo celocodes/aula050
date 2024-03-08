103b358f2c247b9a508c5402acf0c7f6b68a9180
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-03-07 21:06:25 -0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, flash, request
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -11,11 +11,11 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
 class NameForm(FlaskForm):
     nome = StringField('Informe o seu nome:', validators=[DataRequired()])
     sobrenome = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
-    inst = StringField('Informe a sua instituição de ensino:', validators=[DataRequired()])
-    disciplina = SelectField('Informe a sua disciplina:', choices=[('dsw', 'DSWA5'), ('dwb', 'DWBA4'), ('gest', 'Gestão de projetos')], validators=[DataRequired())
+    inst = StringField('Informe a sua Instituição de ensino:', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -32,12 +32,10 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    url = request.host_url
-    ip = request.remote_addr
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, ip=ip, url=url, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'))
